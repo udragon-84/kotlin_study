e5a7023c6b9cebfd2b54c98a8e0acddf63f32cda
DESKTOP-B3MIB1L\user <youzang7@gmail.com>
2025-05-07 10:42:26 +0900

    테스트 코틀린 파일 커밋

---
+++ src/test/kotlin/com/example/kotlin_study/TestCommitKt.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_study
+
+class TestCommitKt {
+    fun print(message: String) {
+        println(message)
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val test = TestCommitKt()
+    test.print("hello")
+}
